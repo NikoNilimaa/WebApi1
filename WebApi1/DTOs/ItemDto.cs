7bbcb5abd71263787128fbe02b944c4f3fe1d472
Wilson <Wilson@NIKO>
2023-05-02 07:12:37 +0300

    Add project files.

---
+++ WebApi1/DTOs/ItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi1.DTOs
+{
+    public record ItemDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
---
+++ WebApi1/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using WebApi1.DTOs;
+using WebApi1.Entities;
+
+namespace WebApi1
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item) 
+        {
+            return new ItemDto
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+                CreatedDate = item.CreatedDate,
+            };
+        }
+    }
+}
